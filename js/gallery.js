d1ee057a0c6411b6bf463459009d61488926cfe0
Catherine Cusick <catherine@cusick.cc>
2022-07-21 17:56:36 -0500

    Apply current class to latest clicked thumbnail

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,7 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
+
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage  = document.querySelector("#gallery-photo > img");
@@ -6,7 +10,14 @@ function activateGallery() {
     thumbnail.addEventListener("click", function() {
       // Set clicked image as main image.
       let newImageSrc = thumbnail.dataset.largeVersion;
+      let newImageAlt = thumbnail.dataset.description;
       mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", newImageAlt);
+
+      // Change which image is current.
+      let currentClass = "current";
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
     });
   });
 }
