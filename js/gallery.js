9f6bca50edb6db399d3d4792b81cb3dd09daded5
Catherine Cusick <catherine@cusick.cc>
2022-07-21 18:06:38 -0500

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -10,14 +10,21 @@ function activateGallery() {
     thumbnail.addEventListener("click", function() {
       // Set clicked image as main image.
       let newImageSrc = thumbnail.dataset.largeVersion;
-      let newImageAlt = thumbnail.dataset.description;
       mainImage.setAttribute("src", newImageSrc);
-      mainImage.setAttribute("alt", newImageAlt);
+      mainImage.setAttribute("alt", thumbnail.alt);
 
       // Change which image is current.
       let currentClass = "current";
       document.querySelector(".current").classList.remove(currentClass);
       thumbnail.parentNode.classList.add(currentClass);
+
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
